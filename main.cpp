7d2ce5f8d3134a656a64423f23ef3e5952bb5b01
AMIR <31338382+amiremohamadi@users.noreply.github.com>
2018-04-20 10:49:38 +0430

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Simple k-map using c++
+ * AMIR MOHAMMADI
+ * Under GPL v3.0 License (general public license)
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  // this is main :)))
+}
